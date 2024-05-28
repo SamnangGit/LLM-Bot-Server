179439ef3c642ad13a1898dc7df6d0e582bb26c4
SamnangGit <phengsamnanggit@gmail.com>
2024-05-28 10:00:02 +0700

    add openai model

--- app/models/chat.py
+++ app/models/chat.py
@@ -1,4 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from configs.config import safety_settings, generation_settings
 from schemas.chat import Message
 import os
@@ -8,11 +11,29 @@ load_dotenv()
 
 class GenerativeModel:
     def __init__(self):
+        self.chat = self.gemini_model()
+        # self.chat = self.openai_model()
+
+
+
+    def gemini_model(self):
         chat = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", 
                                       google_api_key=os.getenv("GEMINI_API_KEY"),
                                       safety_settings=safety_settings,
-                                      temperature=generation_settings['temperature'])
-        self.chat = chat
+                                      temperature=generation_settings['temperature'])    
+        return chat    
+
+    def openai_model(self):
+        template = """Question: {question}
+
+        Answer: Let's think step by step."""
+
+        prompt = PromptTemplate.from_template(template)
+            
+        llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=os.getenv("OPENAI_API_KEY"))
+        llm_chain = prompt | llm
+
+        return llm_chain
 
 
     def start_chat(self, message: Message):
