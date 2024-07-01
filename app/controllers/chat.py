9bb8fc6a70f01a221f11646deb45442197b9c799
SamnangGit <phengsamnanggit@gmail.com>
2024-07-01 07:27:28 +0700

    standardize llm response body

--- app/controllers/chat.py
+++ app/controllers/chat.py
@@ -11,6 +11,20 @@ class ChatController:
         return response
     
 
+    # def start_custom_chat(self, data: dict):
+    #     model = data.get("model")
+    #     message = data.get("message")
+    #     temperature = data.get("temperature")
+    #     top_p = data.get("top_p")
+    #     top_k = data.get("top_k")
+        
+    #     if not message:
+    #         return {"error": "Message content is required"}, 400
+        
+    #     content, token_usage, model_name = self.model.start_custom_chat(model, message, temperature, top_p, top_k)
+    #     return self.format_response(content, token_usage, model_name)
+
+
     def start_custom_chat(self, data):
         model = data.get("model")
         messages = data.get("messages")
@@ -19,3 +33,16 @@ class ChatController:
         top_k = data.get("top_k")
         response = self.model.start_custom_chat(model, messages, temperature, top_p, top_k)
         return response
+
+
+
+    
+
+    def format_response(self, content, token_usage, model_name):
+        return {
+            "content": content,
+            "response_metadata": {
+                "token_usage": token_usage,
+                "model_name": model_name
+            }
+        }
